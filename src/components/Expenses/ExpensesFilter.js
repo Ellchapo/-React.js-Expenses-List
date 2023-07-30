c18c0323e1bdbc5e6116aecb9ac6a83107fe526e
Ellchapo <singh.aakash0000@gmail.com>
2023-07-30 16:36:57 +0530

    commit message

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -17,6 +17,8 @@ const ExpensesFilter = (props) => {
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
           <option value='2019'>2019</option>
+          <option value='2023'>2023</option>
+          <option value='2024'>2024</option>
         </select>
       </div>
     </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,18 +28,18 @@ function ExpenseForm(props){
       setEnteredAmount('');
    };
     return(
-        <form onsSubmit={submitHandler}>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
             <div className='new-expense__control'>
-                <lable>Title</lable>
+                <label>Title</label>
                 <input type='text' value={enteredTitle} onChange= {titleChangeHandler}/>
             </div>
             <div className='new-expense__control'>
-                <lable>Amount</lable>
+                <label>Amount</label>
                 <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
             <div className='new-expense__control'>
-                <lable>Date</lable>
+                <label>Date</label>
                 <input type='date' min="2019-01-01" step="2022-01-01" value={enteredDate} onChange={dateChangeHandler}/>
             </div>
             </div>
